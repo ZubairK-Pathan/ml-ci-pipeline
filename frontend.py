40c564d196cd1af23a8f7ee4ba5773c84da5a8bf
Zubair Khan Pathan <pzubair580@gmail.com>
2026-04-01 11:52:46 +0530

    Test 1

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 
-st.set_page_config(page_title="This is testing", page_icon="🏡", layout="centered")
+st.set_page_config(page_title="Home Price Estimator ", page_icon="🏡", layout="centered")
 
 st.markdown("""
 <h1 style='text-align: center;'>🏡 Home Price Estimator</h1>
